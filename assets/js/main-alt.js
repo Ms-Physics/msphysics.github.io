1f0a7bc7054d07e259a91d71688a05d4d858cdb1
Brandon Skrtich <bskrtich@gmail.com>
2017-01-14 17:35:12 -0700

    Lots-o-updates

--- assets/js/main-alt.js
+++ assets/js/main-alt.js
@@ -27,38 +27,44 @@ if (!window.console) console = {log: function() {}};
         }
 
         // Load course info
-        $.getJSON("/courses/alist.json", function(data) {
+        $.getJSON("/assets/json/courses.json", function(data) {
             var cursemester, curcourses, pastcourses;
+            console.log("course list loaded");
 
-            // Find current semester
-            var cursemester = _.find(data.semesters, function(item) {
-                var startdate = new Date(cdate);
-                var enddate = new Date(cdate);
+            if ($(".load-current-courses").length > 0 ||
+                $(".load-past-courses").length > 0) {
+                // Find current semester
+                var cursemester = _.find(data.semesters, function(item) {
+                    var startdate = new Date(cdate);
+                    var enddate = new Date(cdate);
 
-                startdate.setMonth(item.start.month-1);
-                startdate.setDate(item.start.day);
+                    startdate.setMonth(item.start.month-1);
+                    startdate.setDate(item.start.day);
 
-                enddate.setMonth(item.end.month-1);
-                enddate.setDate(item.end.day);
+                    enddate.setMonth(item.end.month-1);
+                    enddate.setDate(item.end.day);
 
-                return (startdate.getTime() <= cdate.getTime() && cdate.getTime() <= enddate.getTime());
-            });
+                    return (startdate.getTime() <= cdate.getTime() && cdate.getTime() <= enddate.getTime());
+                });
 
-            // Find courses
-            curcourses = _.where(data.courses, {"year": cdate.getFullYear(), "semester": cursemester.name});
-            pastcourses = _.filter(data.courses, function(course) {
-                if (course.year < cdate.getFullYear()) {
-                    return true;
-                } else if (course.year == cdate.getFullYear() &&
-                    data.semester_order[course.semester] < data.semester_order[cursemester.name]) {
-                    return true;
+                // Find courses
+                if ($(".load-current-courses").length > 0) {
+                    curcourses = _.where(data.courses, {"year": cdate.getFullYear(), "semester": cursemester.name});
                 }
 
-                return false;
-            });
-            _.where(data.courses, {"year": cdate.getFullYear(), "semester": curcourses.name});
+                if ($(".load-past-courses").length > 0) {
+                    pastcourses = _.filter(data.courses, function(course) {
+                        if (course.year < cdate.getFullYear()) {
+                            return true;
+                        } else if (course.year == cdate.getFullYear() &&
+                            data.semester_order[course.semester] < data.semester_order[cursemester.name]) {
+                            return true;
+                        }
 
-            console.log(curcourses);
+                        return false;
+                    });
+                }
+            }
 
             // current-courses-tiles
             if ($("#current-courses-tiles").length > 0) {
@@ -74,10 +80,33 @@ if (!window.console) console = {log: function() {}};
             if ($("#past-courses").length > 0) {
                 load_courses_table("#past-courses", pastcourses);
             }
+        }).fail(function() {
+            console.log("error loading courses.json");
         });
 
         function load_current_courses_tiles(tagid, courses) {
+            if (Array.isArray(courses) && courses.length > 0) {
+                $("#no-current-courses-tiles").remove();
 
+                $.each(courses, function(idx, course) {
+                    var tiles = $([
+                        '<article>',
+                        '    <div class="tile-img-container">',
+                        '        <a href="/courses/details.html?id='+course.id+'"><img src="'+course.image+'" alt="" /></a>',
+                        '    </div>',
+                        '    <h3 class="major">',
+                        '        <span class="tile-title-1">'+course.name+'</span><br>',
+                        '        <span class="tile-title-2">'+course.college+', '+course.semester.toTitleCase()+' '+course.year+'</span>',
+                        '    </h3>',
+                        '    <p>'+course.desc+'</p>',
+                        '    <a href="/courses/details.html?id='+course.id+'" class="special">Learn more</a>',
+                        '</article>'
+                    ].join("\n"));
+
+                    $(tagid).append(tiles);
+                });
+
+            }
         }
 
         function load_courses_table(tagid, courses) {
@@ -90,7 +119,7 @@ if (!window.console) console = {log: function() {}};
                         '    <td>'+course.semester.toTitleCase()+' '+course.year+'</td>',
                         '    <td>'+course.college+'</td>',
                         '    <td>'+course.name+'</td>',
-                        '    <td><a href="'+course.file+'">Course Description</a></td>',
+                        '    <td><a href="/courses/details.html?id='+course.id+'">Course Description</a></td>',
                         '</tr>'
                     ].join("\n"));
 
